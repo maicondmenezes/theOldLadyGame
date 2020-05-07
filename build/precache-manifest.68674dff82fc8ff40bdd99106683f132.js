4aa75177e4569257c601b51f8deba4af58ff6e39
Maicon de Menezes <mdmoliveira@inf.ufpel.edu.br>
2020-05-07 20:13:03 -0300

    alterando o icone do logo

---
+++ build/precache-manifest.68674dff82fc8ff40bdd99106683f132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea682cc5c613a1ecbd85c7a0557c6259",
+    "url": "/theOldLadyGame/index.html"
+  },
+  {
+    "revision": "a263170f867aebb170de",
+    "url": "/theOldLadyGame/static/css/main.cafefd92.chunk.css"
+  },
+  {
+    "revision": "7e2f4ff17e82e8ada1c8",
+    "url": "/theOldLadyGame/static/js/2.aa72a190.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theOldLadyGame/static/js/2.aa72a190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a263170f867aebb170de",
+    "url": "/theOldLadyGame/static/js/main.c7b43ad1.chunk.js"
+  },
+  {
+    "revision": "382ce9fe6428a9be882d",
+    "url": "/theOldLadyGame/static/js/runtime-main.f2254c3a.js"
+  }
+]);
